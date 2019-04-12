efce25656670e77025a786d53dc6d3d291eb1341
Luke Niedermyer <luke.niedermyer@socialmedialink.com>
2019-04-12 12:40:22 -0400

    Store data in config variables

--- shuffle.rb
+++ shuffle.rb
@@ -5,5 +5,5 @@ require 'yaml'
 data = YAML.load_file(File.expand_path("./data.yml"))
 
 post('/') do
-  HTTParty.post(data['url'], body: { text: data['team_members'].shuffle.join(', '), channel: "\##{params['channel_name']}", icon_emoji: ':twisted_rightwards_arrows:', username: 'slack-shuffle' }.to_json )
+  HTTParty.post(ENV['WEBHOOK_URL'], body: { text: ENV['TEAM_NAMES'].shuffle.join(', '), channel: "\##{params['channel_name']}", icon_emoji: ENV['ICON_EMOJI'], username: ENV['USERNAME'] }.to_json )
 end
