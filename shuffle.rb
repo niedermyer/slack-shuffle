1f2ebfa6b702c3bf4534ac1cb6a836a7409cc064
Luke Niedermyer <luke.niedermyer@socialmedialink.com>
2019-04-12 12:57:50 -0400

    Update how names are stored

--- shuffle.rb
+++ shuffle.rb
@@ -2,6 +2,14 @@ require 'sinatra'
 require 'httparty'
 require 'yaml'
 
+# ENV['TEAM_NAMES'] stored as ; delimited list of names (i.e. Jane;John;Chris)
+
+
 post('/') do
-  HTTParty.post(ENV['WEBHOOK_URL'], body: { text: ENV['TEAM_NAMES'].shuffle.join(', '), channel: "\##{params['channel_name']}", icon_emoji: ENV['ICON_EMOJI'], username: ENV['USERNAME'] }.to_json )
+  HTTParty.post(ENV['WEBHOOK_URL'], body: { text: text, channel: "\##{params['channel_name']}", icon_emoji: ENV['ICON_EMOJI'], username: ENV['USERNAME'] }.to_json )
+end
+
+def text
+  names = ENV['TEAM_NAMES']..split(/,/)
+  names.shuffle.join(', ')
 end
