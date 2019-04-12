2fe1cc272701ca77db6caf3c67b1634761f04a1f
Luke Niedermyer <luke.niedermyer@socialmedialink.com>
2019-04-12 12:48:54 -0400

    Don’t use data.yml

--- shuffle.rb
+++ shuffle.rb
@@ -2,8 +2,6 @@ require 'sinatra'
 require 'httparty'
 require 'yaml'
 
-data = YAML.load_file(File.expand_path("./data.yml"))
-
 post('/') do
   HTTParty.post(ENV['WEBHOOK_URL'], body: { text: ENV['TEAM_NAMES'].shuffle.join(', '), channel: "\##{params['channel_name']}", icon_emoji: ENV['ICON_EMOJI'], username: ENV['USERNAME'] }.to_json )
 end
