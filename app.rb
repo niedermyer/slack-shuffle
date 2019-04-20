a83b206c67fdc595fabb1fe4531606826151caef
Luke Niedermyer <luke.niedermyer@socialmedialink.com>
2019-04-20 12:08:40 -0400

    Try a response

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
 end
 
+# Slack Ruby client
+$client = Slack::Web::Client.new
+
+
 # ENV['TEAM_NAMES'] stored as ; delimited list of names (i.e. Jane;John;Chris)
 
 # TODO: delete this endpoint and OLD_WEBHOOK_URL after completion of new endpoints
@@ -64,7 +68,7 @@ post('/estimate') do
             "text": "Reveal Estimate"
           },
           "style": "primary",
-          "value": "reveal",
+          "value": estimate,
           "action_id": "reveal"
         },
         {
@@ -110,6 +114,176 @@ end
 post('/response') do
   logger.info('I GOT A RESPONSE!!!!')
   log(message: params, delimiter: '-')
+
+  request_data = JSON.parse(request.body.read)
+  action = request_data['actions'][0]
+
+  case action['action_id']
+  when 'reveal'
+    estimate = action['value']
+    blocks = [
+      {
+        "type": "context",
+        "elements": [
+          {
+            "type": "mrkdwn",
+            "text": "*Author:* #{user_real_name}"
+          }
+        ]
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": estimate
+        }
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Delete"
+            },
+            "style": "danger",
+            "value": "delete",
+            "action_id": "delete",
+            "confirm": {
+              "title": {
+                "type": "plain_text",
+                "text": "Delete #{user_real_name}'s Estimate?"
+              },
+              "text": {
+                "type": "mrkdwn",
+                "text": "Are you sure that you want to delete this estimate?"
+              },
+              "confirm": {
+                "type": "plain_text",
+                "text": "Yes, delete it!"
+              },
+              "deny": {
+                "type": "plain_text",
+                "text": "Nevermind, I'll keep it"
+              }
+            }
+          }
+        ]
+      }
+    ]
+
+    body = { blocks: blocks }.to_json
+
+    HTTParty.post(request_data['response_url'], body: body )
+  when 'delete'
+    ts = request_data['message']['ts']
+
+  end
+  # response = {
+  #   payload: {
+  #     type: "block_actions",
+  #     team: {
+  #       id: "T0DRJ2NQ4",
+  #       domain: "socialmedialink"
+  #     },
+  #     user: {
+  #       id: "UC8TB7B46",
+  #       username: "luke.niedermyer",
+  #       name: "luke.niedermyer",
+  #       team_id: "T0DRJ2NQ4"
+  #     },
+  #     api_app_id: "AHQ9EDG1Z",
+  #     token: "4tOjWhIZ2l4XL1uGdnu8RI1i",
+  #     container: {
+  #       type: "message",
+  #       message_ts: "1555771432.000600",
+  #       channel_id: "DC6RU5N56",
+  #       is_ephemeral: false
+  #     },
+  #     trigger_id: "615919702295.13868090820.fe5580f5fc17bb55b930fef761f6d09a",
+  #     channel: {
+  #       id: "DC6RU5N56",
+  #       name: "directmessage"
+  #     },
+  #     message: {
+  #       type: "message",
+  #       subtype: "bot_message",
+  #       text: "Luke Niedermyer ********",
+  #       ts: "1555771432.000600",
+  #       bot_id: "BHQBH91FC",
+  #       blocks: [
+  #         {
+  #           type: "context",
+  #           block_id: "N4ul",
+  #           elements: [
+  #             { type: "mrkdwn", text: "*Author:* Luke Niedermyer", verbatim: false }
+  #           ]
+  #         },
+  #         {
+  #           type: "divider",
+  #           block_id: "XLR/+"
+  #         },
+  #         {
+  #           type: "section",
+  #           block_id: "253",
+  #           text: { type: "mrkdwn", text: "********", verbatim: false }
+  #         },
+  #         {
+  #           type: "actions",
+  #           block_id: "jl+s",
+  #           elements: [
+  #             {
+  #               type: "button",
+  #               action_id: "reveal",
+  #               text: {
+  #                 type: "plain_text",
+  #                 text: "Reveal Estimate",
+  #                 emoji: true
+  #               },
+  #               style: "primary",
+  #               value: "reveal"
+  #             },
+  #             {
+  #               type: "button",
+  #               action_id: "delete",
+  #               text: {
+  #                 type: "plain_text",
+  #                 text: "Delete",
+  #                 emoji: true
+  #               },
+  #               style: "danger",
+  #               value: "delete",
+  #               confirm: {
+  #                 title: { type: "plain_text", text: "Delete Luke Niedermyer 's Estimate?", emoji: true }, text: { type: "mrkdwn", text: "Are you sure that you want to delete this estimate?", verbatim: false }, confirm: { type: "plain_text", text: "Yes, delete it!", emoji: true }, "deny": { "type": "plain_text", text: "Nevermind, I' ll keep it", emoji: true }
+  #               }
+  #             }
+  #           ]
+  #         }
+  #       ]
+  #     },
+  #     response_url: "https://hooks.slack.com/actions/T0DRJ2NQ4/602689458274/lVd1sCrwqQXrEeeaNGSTOgdJ",
+  #     actions: [
+  #       {
+  #         action_id: "reveal",
+  #         block_id: "jl+s",
+  #         text: {
+  #           type: "plain_text",
+  #           text: "Reveal Estimate",
+  #           emoji: true
+  #         },
+  #         value: "reveal",
+  #         type: "button",
+  #         style: "primary",
+  #         action_ts: "1555771550.596900"
+  #       }
+  #     ]
+  #   }
+  # }
+  status 200
 end
 
 
